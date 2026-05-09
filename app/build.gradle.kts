8c4bdc49231563304cbcb1185411cf9baa9ebb46
jahes <hellgate241@gmail.com>
2026-05-09 22:17:00 +0800

    memasang dependency yang diperlukan

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,11 @@ android {
             )
         }
     }
+
+    buildFeatures{
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -40,6 +45,28 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    // --- Tambahan Library Proyek Tim ---
+
+    // Networking & API (Tugas Faqih)
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.converter.gson)
+    implementation(libs.okhttp.core)
+    implementation(libs.okhttp.logging)
+    implementation(libs.google.gson)
+
+    implementation(libs.androidx.lifecycle.viewmodel)
+    implementation(libs.androidx.lifecycle.livedata)
+
+    // Navigation (Tugas Latifah)
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.ui)
+
+    // Image Loading (Tugas Salsabila/Latifah)
+    implementation(libs.glide.core)
+    annotationProcessor(libs.glide.compiler) // Penting: Tetap pakai annotationProcessor untuk Java
+
+    // Testing (Sesuai Screenshot Anda)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
